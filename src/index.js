4c7ca356539f02e3b0d167c4336f86e3e4cddd23
dewslyse <dewslyse@gmail.com>
2022-04-05 21:51:40 +0000

    Render sample scores to page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.scss';
+import sampleScores from './modules/sample.js';
+
+const scoresList = document.querySelector('.scores-list');
+
+const score = (item) => `
+    <div class="score-box">
+        <p class="score">${item.name}: <span class="number">${item.score}</span></p>
+    </div>
+`;
+
+scoresList.innerHTML = `${sampleScores.map(score).join('')}`;
