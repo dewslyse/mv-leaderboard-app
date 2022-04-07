5a5c57f06772c930a7e33a1ffccfc6d0d2f89fe2
dewslyse <dewslyse@gmail.com>
2022-04-07 18:37:24 +0000

    Change event action on form to submit

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ import './style.scss';
 import * as APIs from './modules/api.js';
 
 const refreshBtn = document.querySelector('.refresh-btn');
-const addBtn = document.querySelector('.add-btn');
 const form = document.querySelector('.form');
 
 document.addEventListener('DOMContentLoaded', APIs.displayScores);
 
-addBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  const userName = document.getElementById('add-name').value;
-  const userScore = document.getElementById('add-score').value;
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userName = document.getElementById('add-name').value;
+    const userScore = document.getElementById('add-score').value;
 
-  APIs.addScore(userName, userScore);
-  form.reset();
+    APIs.addScore(userName, userScore);
+    form.reset();
 });
 
 refreshBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  APIs.displayScores();
+    e.preventDefault();
+    APIs.displayScores();
 });
