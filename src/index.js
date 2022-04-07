524ae4c60bad3c377eedbd6bd2080d3e06defc5c
dewslyse <dewslyse@gmail.com>
2022-04-07 10:45:02 +0000

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.scss';
-import * as APIs from './modules/api.js'
+import * as APIs from './modules/api.js';
 
 const refreshBtn = document.querySelector('.refresh-btn');
 const addBtn = document.querySelector('.add-btn');
@@ -8,15 +8,15 @@ const form = document.querySelector('.form');
 document.addEventListener('DOMContentLoaded', APIs.displayScores);
 
 addBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    let userName = document.getElementById('add-name').value;
-    let userScore = document.getElementById('add-score').value;
+  e.preventDefault();
+  const userName = document.getElementById('add-name').value;
+  const userScore = document.getElementById('add-score').value;
 
-    APIs.addScore(userName, userScore);
-    form.reset();
+  APIs.addScore(userName, userScore);
+  form.reset();
 });
 
 refreshBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    APIs.displayScores();
+  e.preventDefault();
+  APIs.displayScores();
 });
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,54 +1,54 @@
 const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
 const id = 'wiA4aOSO2J5kDF8jHBi1';
 
-//Get  game id
+// Get  game id
 const newGame = async () => {
-    const response = await fetch(baseURL, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name: 'Game of Games' }),
-    });
-    const data = await response.json();
-    return data;
+  const response = await fetch(baseURL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name: 'Game of Games' }),
+  });
+  const data = await response.json();
+  return data;
 };
 
-//Create user score
+// Create user score
 const addScore = async (user, score) => {
-    const response = await fetch(`${baseURL}/${id}/scores/`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            user,
-            score: Number(score)
-        }),
-    });
-    const res = await response.json();
-    return res;
+  const response = await fetch(`${baseURL}/${id}/scores/`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      user,
+      score: Number(score),
+    }),
+  });
+  const res = await response.json();
+  return res;
 };
 
-//Retrieve user scores
+// Retrieve user scores
 const retrieveScores = async () => {
-    const response = await fetch(`${baseURL}/${id}/scores/`);
-    const allScores = await response.json();
-    return allScores;
+  const response = await fetch(`${baseURL}/${id}/scores/`);
+  const allScores = await response.json();
+  return allScores;
 };
 
 const displayScores = async (userData) => {
-    const scoresList = document.querySelector('.scores-list');
+  const scoresList = document.querySelector('.scores-list');
 
-    userData = await retrieveScores();
-    const sortedData = userData.result.sort((a, b) => b.score - a.score);
+  userData = await retrieveScores();
+  const sortedData = userData.result.sort((a, b) => b.score - a.score);
 
-    scoresList.innerHTML = '';
+  scoresList.innerHTML = '';
 
-    sortedData.forEach((item) => {
-        const score = `
+  sortedData.forEach((item) => {
+    const score = `
             <div class="score-box">
                 <p class="score">${item.user}: <span class="number">${item.score}</span></p>
             </div>
         `;
-        scoresList.innerHTML += score;
-    });
-}
+    scoresList.innerHTML += score;
+  });
+};
 
-export { addScore, retrieveScores, displayScores };
+export { newGame, addScore, displayScores };
