86f22e7c772777020eb65939fee6df06d5817a56
dewslyse <dewslyse@gmail.com>
2022-04-07 00:09:57 +0000

    Import api functions into index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import './style.scss';
 import sampleScores from './modules/sample.js';
+import * as APIs from './modules/api.js'
 
-const scoresList = document.querySelector('.scores-list');
+// const scoresList = document.querySelector('.scores-list');
 
-const score = (item) => `
-    <div class="score-box">
-        <p class="score">${item.name}: <span class="number">${item.score}</span></p>
-    </div>
-`;
+// const score = (item) => `
+//     <div class="score-box">
+//         <p class="score">${item.name}: <span class="number">${item.score}</span></p>
+//     </div>
+// `;
 
-scoresList.innerHTML = `${sampleScores.map(score).join('')}`;
+// scoresList.innerHTML = `${sampleScores.map(score).join('')}`;
